a2d71175cef618e15f1e9ea29a339f86015d9b5e
solowihmaria <solowihmaria@gmail.com>
2024-12-05 21:25:20 +0300

    feat: solve 7th task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -125,8 +125,11 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const sum = arr.reduce((acc, num) => acc + num, 0);
+  const average = sum / arr.length;
+  return Number(average.toFixed(2));
 }
 
 /**
