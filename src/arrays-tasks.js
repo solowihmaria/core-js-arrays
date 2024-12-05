cb094dc8ffec45ccb9ccb13b1cedf26b46f7e6b3
solowihmaria <solowihmaria@gmail.com>
2024-12-05 21:36:50 +0300

    feat: solve 8th task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -142,8 +142,10 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  if (arr.length === 0) return true;
+  const firstLength = arr[0].length;
+  return arr.every((str) => str.length === firstLength);
 }
 
 /**
