71dbddc607678be038c642d32055c60c25e422fb
solowihmaria <solowihmaria@gmail.com>
2024-12-09 15:34:29 +0300

    feat: solve 26th task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -475,8 +475,10 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  if (arr.length === 0) return [];
+  const ordered = arr.sort((a, b) => b - a);
+  return ordered.slice(0, n);
 }
 
 /**
